d1be18bb5b4d13126be7e8228abc347398f227ce
Oscar Caceres <oscarcaceres9012@gmail.com>
2023-08-26 15:08:31 -0500

    Cree una carpeta para los ejercicios pero he hecho uno

---
+++ src/clases/01-datos/practica/oscarejercicios/index.js
@@ -0,0 +1,9 @@
+//Ejercicio #2
+//Extrae en variables el primer y cuarto elemento.
+//- Extrae en variables `email` y `mascotas` del primer elemento.
+
+// Destructuracion:
+
+  const [first, , , fourth] = users
+
+  const { email, mascotas } = first
